79f92b2e87c9e41e9f35f86e5992542ce681d18f
Ali Shaibu <alishaibu2002@gmail.com>
2018-07-26 23:44:54 +0100

    created a signin endpoint

--- server/routes/users.js
+++ server/routes/users.js
@@ -2,5 +2,6 @@ import userController from '../controllers/users';
 
 
 export default (app) => {
-  app.post('/api/v1/users', userController.signup);
+  app.post('/api/v1/auth/signup', userController.signup);
+  app.post('/api/v1/auth/login', userController.signin);
 };
