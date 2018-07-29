c71f3169e255e593304d9dd5c7e934d4d2fd3645
Ali Shaibu <alishaibu2002@gmail.com>
2018-07-29 22:45:38 +0100

    Create an Endpoint that Modifies an Entry

--- server/controllers/entries.js
+++ server/controllers/entries.js
@@ -26,14 +26,48 @@ const addEntry = (req, res) => {
           return res.status(500).send({ error: err.message });
         }
         const data = result.rows[0];
-        return res.status(200).send({ message: 'Entry successfully created and added!', data });
+        return res.status(200).send({ success: true, message: 'Entry successfully created and added!', data });
       }
 
     );
   });
 };
 
+const modifyEntry = (req, res) => {
+  const id = req.params.entryId;
+  const currentDate = `${new Date()}`;
+  const entry = {
+    title: req.body.title,
+    content: req.body.content,
+    createdAt: currentDate.slice(0, 24),
+    updatedAt: currentDate.slice(0, 24)
+  };
+  pg.connect(connectionString, (err, client, done) => {
+    // Handle connection errors
+    if (err) {
+      done();
+      return res.status(500).json({ success: false, data: err });
+    }
+    // SQL Query > Update Data
+    client.query(
+      'UPDATE Entries SET title=($1), content=($2), createdAt=($3), updatedAt=($4) WHERE id=($5) RETURNING id, title, content, createdAt, updatedAt',
+      [entry.title, entry.content, entry.createdAt, entry.updatedAt, id],
+      (err, result) => {
+        if (err) {
+          return res.status(500).send({ error: err.message });
+        }
+        if (!result.rows[0]) {
+          return res.status(404).send({ success: false, message: 'Entry not found!' });
+        }
+        const data = result.rows[0];
+        return res.status(200).send({ success: true, message: 'Entry successfully updated!', data });
+      }
+    );
+  });
+};
+
 
 export default {
-  addEntry
+  addEntry,
+  modifyEntry
 };
