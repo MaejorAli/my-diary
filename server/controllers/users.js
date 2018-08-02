a531d332cb899fb8eed601c56a94fbbd8233a7b7
Ali Shaibu <alishaibu2002@gmail.com>
2018-08-02 10:40:06 +0100

    modified travis.yml

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -8,7 +8,7 @@ dotenv.config();
 const secret = process.env.SECRET;
 
 
-const connectionString = process.env.DB_URL || 'postgres://postgres:ali1702@127.0.0.1:5432/my-diary';
+const connectionString = process.env.DB_URL;
 
 const signup = (req, res) => {
   const currentDate = `${new Date()}`;
