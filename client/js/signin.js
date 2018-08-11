5dbded28218f072687658ee7de44b30863c642b6
Ali Shaibu <alishaibu2002@gmail.com>
2018-08-11 13:40:03 +0100

    created signin front end functionality

---
+++ client/js/signin.js
@@ -0,0 +1,51 @@
+const emailField = document.getElementById('email');
+const passwordField = document.getElementById('password');
+const enterSignIn = document.getElementById('enter-signin');
+const errorField = document.getElementById('errors');
+
+
+const userToken = '';
+
+const signin = (event) => {
+  event.preventDefault();
+
+  const url = 'http://127.0.0.1:5000/api/v1/auth/login';
+  const email = emailField.value.trim();
+  const password = passwordField.value.trim();
+
+  const signInCred = {
+    email,
+    password,
+  };
+
+  const fetchData = {
+    method: 'POST',
+    body: JSON.stringify(signInCred),
+    headers: {
+      Accept: 'application/json, text/plain,  */*',
+      'Content-type': 'application/json',
+    },
+  };
+
+  fetch(url, fetchData)
+    .then(res => res.json())
+    .then((result) => {
+      if (result.message === 'You have successfully signed in') {
+        window.localStorage.setItem('token', JSON.stringify(result.token));
+
+        window.location.href = '../client/entries.html';
+      } else {
+        throw new Error(result.error);
+      }
+    })
+    .catch((error) => {
+      errorField.innerHTML = error;
+    });
+};
+
+
+window.onload = () => {
+  if (enterSignIn !== null) {
+    enterSignIn.addEventListener('click', signin);
+  }
+};
